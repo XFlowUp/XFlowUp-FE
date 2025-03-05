d9eee05fa0f8b92447bc82e78cc43174c68371fb
Luong Tuan Anh <dulieu.vblc@gmail.com>
2025-03-05 11:48:15 +0700

    add: show email on dashboard

--- src/app/(fe)/(dashboard)/dashboard/page.tsx
+++ src/app/(fe)/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+"use client";
 
+import React from 'react';
+import useAuth from '@/shared/hooks/useAuth';
 
 export default function DashboardPage() {
+  const { user, isAuthenticated } = useAuth();
+
   return (
     <div>
       <h1>Dashboard</h1>
+      {isAuthenticated && <p>Email: {user?.email}</p>}
     </div>
   );
 }
